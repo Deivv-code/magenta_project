9978ef3c2c0100318ae50f383006642c29936747
Davide Davide <dvdca@DESKTOP-7RQLGD1.(none)>
2022-05-31 10:52:37 +0200

    resolve problem

---
+++ JavaProjectMagenta/src/main/java/Main.java
@@ -0,0 +1,72 @@
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        if (args.length == 0 || args.length == 1) {
+            System.out.println("Devi scrivere due parametri: ");
+            return;
+        }
+        String file_name = args[0];
+        String sensor_name = args[1];
+        System.out.println(args);
+
+        String currentDirectory = System.getProperty("user.dir");
+        System.out.println(currentDirectory + "\\" + file_name);
+        String filePath = currentDirectory + "\\files\\" + file_name;
+        BufferedReader in = null;
+        String line ="";
+        try {
+            in = new BufferedReader(new FileReader(filePath));
+            while((line = in.readLine() )!= null)
+            {
+                String[] row = line.split(",");
+
+                for(String index : row)
+                {
+                    System.out.printf("%10s", index);
+                }
+                System.out.println();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+    /*    try {
+            in = new FileReader(filePath);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        Iterable<CSVRecord> records;
+        try {
+            records = CSVFormat.EXCEL.parse(in);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (CSVRecord record : records) {
+            String datetime = record.get("datetime");
+            String latitude = record.get("latitude");
+            String longitude = record.get("longitude");
+            String sensor_type = record.get("sensor_type");
+            Long value = Long.parseLong(record.get("value"));
+
+        }*/
+
+    }
+}
